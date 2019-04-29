a79998c836b8582f599af9da3de7ff9844d1c27e
oanpeas1 <50068110+oanpeas1@users.noreply.github.com>
2019-04-29 11:13:17 +0700

    Add files via upload

--- firstbot.php
+++ firstbot.php
@@ -33,6 +33,10 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
             $replyToken = $event['replyToken']; //เก็บ reply token เอาไว้ตอบกลับ
             $source_type = $event['source']['type'];//เก็บที่มาของ event(user หรือ group)
             $txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
+            if($txtin == 'บัดซบ')
+            {
+                $txtback = 'จริงๆเลย';
+            }
             reply_msg($content,$replyToken);      
         }
     }
