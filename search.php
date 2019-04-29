a19e6f5664856300201e55f93498705842eef087
oanpeas1 <50068110+oanpeas1@users.noreply.github.com>
2019-04-29 15:31:38 +0700

    Add files via upload

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
     require('con_db.php');
-    $sql_text = "SELECT * FROM tbl_test WHERE keyword LIKE 'a'";
+    $sql_text = "SELECT * FROM tbl_test WHERE keyword LIKE '%a%'";
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
